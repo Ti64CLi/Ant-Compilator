408394741ee7a9b244b2d223a5aeed3bd469540f
Adrien <decosse.adrien@gmail.com>
2022-10-21 09:55:45 +0200

    Prise en compte du changement de val d'un bit

--- test/tests naifs/decompressor/decompressor.py
+++ test/tests naifs/decompressor/decompressor.py
@@ -1,25 +1,33 @@
-A = {'A':'A','B':'B','C':'C','D':'D','E':'E','F':'F'}
-B = {'A':'B','B':'C','C':'D','D':'E','E':'F','F':'A'}
-C = {'A':'C','B':'D','C':'E','D':'F','E':'A','F':'B'}
-D = {'A':'D','B':'E','C':'F','D':'A','E':'B','F':'C'}
-E = {'A':'E','B':'F','C':'A','D':'B','E':'C','F':'D'}
-F = {'A':'F','B':'A','C':'B','D':'C','E':'D','F':'E'}
+A = {'A':'A','B':'B','C':'C','D':'D','E':'E','F':'F','00':0}
+B = {'A':'B','B':'C','C':'D','D':'E','E':'F','F':'A','00':1}
+C = {'A':'C','B':'D','C':'E','D':'F','E':'A','F':'B','00':2}
+D = {'A':'D','B':'E','C':'F','D':'A','E':'B','F':'C','00':3}
+E = {'A':'E','B':'F','C':'A','D':'B','E':'C','F':'D','00':4}
+F = {'A':'F','B':'A','C':'B','D':'C','E':'D','F':'E','00':5}
 
 dfichier = open("decompressed-code","w")
 
 for d in [A,B,C,D,E,F]:
+
     fichier = open("code","r")
     c = " "
-    w = False
+    commit = False
+
     while c:
         c = fichier.read(1)
-        if w:
-            dfichier.write(d[c])
-            w = False
-        elif c == '#':
-            w = True
-        else:
-            dfichier.write(c)
+        if c == '*':
+            commit = not commit
+
+        if c == '!':
+            c = fichier.read(2)
+            dfichier.write(str((d['00']+int(c))%6))
+        elif not commit and c != '*':
+            if c == '#':
+                c = fichier.read(1)
+                dfichier.write(d[c])
+            else :
+                dfichier.write(c)
+
     fichier.close()
 
 
